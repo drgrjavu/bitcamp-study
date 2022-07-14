652771174f4408b7ddc8f4ac7a85e90e640a51c0
Jongchan Choi <drgrjongchan@gmail.com>
2022-07-14 10:25:03 +0900

    공부자료

--- board-app/app/src/main/java/com/bitcamp/board/App.java
+++ board-app/app/src/main/java/com/bitcamp/board/App.java
@@ -17,8 +17,13 @@ public class App {
     System.out.println();
 
     java.util.Scanner keyboardInput = new java.util.Scanner(System.in);
+    
+    //날짜 정보에서 값을 추출하여 특정 포맷의 문자열로 만들어줄 도구를 준비
+    java.text.SimpleDateFormat formatter =
+    new java.text.SimpleDateFormat("yyyy-MM-dd");
 
     final int SIZE = 5; //불변하는final은 대문자로 강조표기하여 식별가능하게한다.
+    
     int[] no = new int[SIZE];
     String[] title = new String[SIZE];
     String[] content = new String[SIZE];
@@ -57,9 +62,7 @@ public class App {
         System.out.println("[게시글 목록]");
         System.out.println("번호 제목 조회수 작성자 등록일");
         
-        //날짜 정보에서 값을 추출하여 특정 포맷의 문자열로 만들어줄 도구를 준비
-        java.text.SimpleDateFormat formatter =
-          new java.text.SimpleDateFormat("yyyy-MM-dd");
+        
 
         for (int i = 0; i < boardCount; i++) {
           // 밀리초 데이터 ==> Date 도구함 설정
