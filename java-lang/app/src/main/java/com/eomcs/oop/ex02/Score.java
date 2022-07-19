67ee2a6a0af7c73087c90021200b55100b75f5b9
Jongchan Choi <drgrjongchan@gmail.com>
2022-07-19 17:01:33 +0900

    공부자료

--- java-lang/app/src/main/java/com/eomcs/oop/ex02/Score.java
+++ java-lang/app/src/main/java/com/eomcs/oop/ex02/Score.java
@@ -1,5 +1,6 @@
 package com.eomcs.oop.ex02;
 
+// 다른 패키지에서도 사용할 수 있도록 public 으로 공개한다.
 public class Score {
   String name; 
   int kor;
@@ -8,6 +9,7 @@ public class Score {
   int sum;
   float aver;
 
+  // static method ==> instance method
   void compute() {
     // 인스턴스 메서드를 호출할 때 넘겨준 인스턴스를 주소는
     // this 라면 내장 변수(built-in)에 보관된다.
