0f389ea817a12a0b12284cef8d9ea168904f98d4
Jongchan Choi <drgrjongchan@gmail.com>
2022-07-20 13:02:59 +0900

    공부자료

--- java-lang/app/src/main/java/com/eomcs/oop/ex02/test/ExamTest.java
+++ java-lang/app/src/main/java/com/eomcs/oop/ex02/test/ExamTest.java
@@ -1,7 +1,7 @@
 package com.eomcs.oop.ex02.test;
 
 //0) 낱개 변수 사용
-//1) 성적 데이터를 저장할 사용자 정의 데이터 타입을 만든다.`
+//1) 성적 데이터를 저장할 사용자 정의 데이터 타입을 만든다.
 //2) 리팩토링: 메서드 추출(extract method), static nested class
 //3) 리팩토링: 메서드 추출(extract method) = 한 개의 메서드는 한 개의 기능을 수행해야 한다.
 //4) GRASP(General Responsibility Assignment Software Patterns) 패턴 
@@ -26,16 +26,17 @@ package com.eomcs.oop.ex02.test;
 //8) 생성자 도입: 인스턴스를 생성할 때 값을 초기화시키는 특별한 메서드
 //
 public class ExamTest {
-  public static void main(String[] args) {
 
-    class Score {
-      String name;
-      int kor;
-      int eng;
-      int math;
-      int sum;
-      float aver;
-    }
+  static class Score {
+    String name;
+    int kor;
+    int eng;
+    int math;
+    int sum;
+    float aver;
+  }
+
+  public static void main(String[] args) {
 
     Score s1 = new Score();
 
@@ -43,11 +44,7 @@ public class ExamTest {
     s1.kor = 100;
     s1.eng = 90;
     s1.math = 85;
-    s1.sum = s1.kor + s1.eng + s1.math;
-    s1.aver = (float) s1.sum / 3;
-
-    System.out.printf("%s: %d, %d, %d, %d, %.1f\n", 
-        s1.name, s1.kor, s1.eng, s1.math, s1.sum, s1.aver);
+    printScore(s1);
 
     Score s2 = new Score();
 
@@ -55,11 +52,7 @@ public class ExamTest {
     s2.kor = 90;
     s2.eng = 80;
     s2.math = 75;
-    s2.sum = s2.kor + s2.eng + s2.math;
-    s2.aver = (float) s2.sum / 3;
-
-    System.out.printf("%s: %d, %d, %d, %d, %.1f\n", 
-        s2.name, s2.kor, s2.eng, s2.math, s2.sum, s2.aver);
+    printScore(s2);
 
     Score s3 = new Score();
 
@@ -67,10 +60,15 @@ public class ExamTest {
     s3.kor = 80;
     s3.eng = 70;
     s3.math = 65;
-    s3.sum = s3.kor + s3.eng + s3.math;
-    s3.aver = (float) s3.sum / 3;
+    printScore(s3);
+  }
+
+  static void printScore(Score s) {
+    s.sum = s.kor + s.eng + s.math;
+    s.aver = (float) s.sum / 3;
 
     System.out.printf("%s: %d, %d, %d, %d, %.1f\n", 
-        s3.name, s3.kor, s3.eng, s3.math, s3.sum, s3.aver);
+        s.name, s.kor, s.eng, s.math, s.sum, s.aver);
   }
+
 }
