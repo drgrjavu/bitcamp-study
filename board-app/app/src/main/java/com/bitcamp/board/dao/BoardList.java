9177fc8a2e1e3fb36136e480129ef48ddcb9764d
Jongchan Choi <drgrjongchan@gmail.com>
2022-07-22 16:41:33 +0900

    공부자료

--- board-app/app/src/main/java/com/bitcamp/board/dao/BoardList.java
+++ board-app/app/src/main/java/com/bitcamp/board/dao/BoardList.java
@@ -4,66 +4,55 @@ import com.bitcamp.board.domain.Board;
 
 // 게시글 목록을 관리하는 역할
 //
-public class BoardList {
+public class BoardList extends ObjectList {
 
   private int no = 0;
 
 
+  //수퍼 클래스의 get() 메서드는 인덱스로 항목을 찾는다.
+  // 그래서 Board 객체를 다루기에 적합하지 않다.
+  // 따라서 다음 메서드처럼 Board 객체를 조회하는데 적합한 메서드를 추가한다.
+  // 이 메서드는 게시글 번호에 해당하는 Board 인스턴스를 찾아 리턴한다.
+  @Override
+  public Board getByBoardNo(int boardNo) {
 
-  // 목록에 저장된 인스턴스를 꺼내서 리턴한다.
+    //수퍼 클래스의 add(Object)대신 Board 객체를 저장할 수 있도록
+    // 같은 이름의 유사 기능을 수행하는 메서드를 추가 정의한다. => Overloading 이라 부른다.
+    public void add(Board board) {
+      board.no = nextNo();
 
-  // 게시글 번호에 해당하는 Board 인스턴스를 찾아 리턴한다.
-  public Board get(int boardNo) {
-    for (int i = 0; i < this.boardCount; i++) {
-      if (this.boards[i].no == boardNo) {
-        return this.boards[i];
+      super.add(board);
+    }
+
+    public boolean removeByBoardNo(int boardNo) {
+      int boardIndex = -1;
+
+      // 삭제할 항목의 다음 항목을 앞으로 당긴다.
+      for (int i = index + 1; i < this.length; i++) {
+        this.list[i - 1] = this.list[i];
+      }
+
+      // 게시글 개수를 한 개 줄인 후 
+      // 맨 뒤의 있던 항목의 주소를 0으로 설정한다.
+      this.list[--this.length] = null;
+      return true;
+    }
+
+    for (int i = 0; i < this.length; i++) {
+      //Object 배열에 실제 들어 있는 것은 Board라고 컴파일러에게 알린다.
+      Board board = (Board) this.list[i]; 
+
+      if (board.no == boardNo) {
+        return board;
       }
     }
     return null;
   }
 
   // Board 인스턴스를 배열에 저장한다.
-  public void add(Board board) {
-    if (this.boardCount == this.boards.length) {
-      grow();
-    }
-    board.no = nextNo();
-    this.boards[this.boardCount++] = board;
-  }
 
-  public boolean remove(int boardNo) {
-    int boardIndex = -1;
-    for (int i = 0; i < this.boardCount; i++) {
-      if (this.boards[i].no == boardNo) {
-        boardIndex = i;
-        break;
-      }
-    }
 
-    if (boardIndex == -1) {
-      return false;
-    }
 
-    // 삭제할 항목의 다음 항목을 앞으로 당긴다.
-    for (int i = boardIndex + 1; i < this.boardCount; i++) {
-      this.boards[i - 1] = this.boards[i];
-    }
-
-    // 게시글 개수를 한 개 줄인 후 
-    // 맨 뒤의 있던 항목의 주소를 0으로 설정한다.
-    this.boards[--this.boardCount] = null;
-
-    return true;
-  }
-
-  private void grow() {
-    int newSize = this.boards.length + (this.boards.length >> 1);
-    Board[] newArray = new Board[newSize];
-    for (int i = 0; i < this.boards.length; i++) {
-      newArray[i] = this.boards[i];
-    }
-    this.boards = newArray;
-  }
 
   private int nextNo() {
     return ++no;
--- board-app/app/src/main/java/com/bitcamp/board/dao/ObjectList.java
+++ board-app/app/src/main/java/com/bitcamp/board/dao/ObjectList.java
@@ -3,8 +3,10 @@ package com.bitcamp.board.dao;
 public class ObjectList {
   private static final int DEFAULT_SIZE = 3;
 
-  private int length; 
-  private Object[] list; 
+  //서브 클래스에서 직접 접근하 수 있도록 접근 범위를 넓힌다.
+  protected int length; 
+  protected Object[] list; //protected: 같은 패키지 + 서브 클래스
+
   public ObjectList() {
     this.list = new Object[DEFAULT_SIZE];
   }
@@ -27,9 +29,39 @@ public class ObjectList {
     return list[index];
   }
 
+  public void add(Object obj) {
+    if (this.length == this.list.length) {
+      grow();
+    }
+    this.list[this.length++] = obj;
+  }
 
+  private void grow() {
+    int newSize = this.list.length + (this.list.length >> 1);
+    Object[] newArray = new Object[newSize];
+    for (int i = 0; i < this.list.length; i++) {
+      newArray[i] = this.list[i];
+    }
+    this.list = newArray;
+  }
 
+  public boolean remove(int index) {
+    if (index < 0 || index >= this.length) {
+      return false;
+    }
+
+    // 삭제할 항목의 다음 항목을 앞으로 당긴다.
+    for (int i = index + 1; i < this.length; i++) {
+      this.list[i - 1] = this.list[i];
+    }
+
+    // 게시글 개수를 한 개 줄인 후 
+    // 맨 뒤의 있던 항목의 주소를 0으로 설정한다.
+    this.list[--this.length] = null;
+    return true;
+  }
 }
+
 
 
 
