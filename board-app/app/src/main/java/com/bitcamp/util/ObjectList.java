9bb846ea929cac2aa621b492ddde08c8325c62bd
Jongchan Choi <drgrjongchan@gmail.com>
2022-07-27 11:59:46 +0900

    공부자료

---
+++ board-app/app/src/main/java/com/bitcamp/util/ObjectList.java
@@ -0,0 +1,62 @@
+package com.bitcamp.util;
+
+public class ObjectList {
+  private static final int DEFAULT_CAPACITY = 10;
+
+  private int size;
+  private Object[] elementData;
+
+  public ObjectList() {
+    elementData = new Object[DEFAULT_CAPACITY];
+  }
+
+  public ObjectList(int initialCapacity) {
+    elementData = new Object[initialCapacity];
+  }
+
+  public void add(Object e) {
+    if (size == elementData.length) {
+      grow();
+    }
+
+    elementData[size++] = e;
+  }
+
+  public Object[] toArray() {
+    Object[] arr = new Object[size];
+    for (int i = 0; i < arr.length; i++) {
+      arr[i] = elementData[i];
+    }
+    return arr;
+  }
+
+  public Object get(int index) {
+    if (index < 0 || index >= size) {
+      return null;
+    }
+    return elementData[index];
+  }
+
+  public boolean remove(int index) {
+    if(index < 0 || index >= size) {
+      return false;
+    }
+
+    for (int i = index + 1; i < size; i ++) {
+      elementData[i -1] = elementData[i];
+    }
+    elementData[--size] = null;
+    return;
+
+    private void grow() {
+      int newCapacity = elementData.length + (elementData.length >> 1);
+      Object[] newArray = new Object[newCapacity];
+      for (int i = 0; i < elementData.length; i++) {
+        newArray[i] = elementData[i];
+      }
+      elementData = newArray;
+    }
+
+
+  }
+
