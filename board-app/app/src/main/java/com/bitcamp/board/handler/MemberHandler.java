daea6f154aaa780fbebbd2cb95feb5da8bfac3ce
Jongchan Choi <drgrjongchan@gmail.com>
2022-07-28 17:34:50 +0900

    공부자료

--- board-app/app/src/main/java/com/bitcamp/board/handler/MemberHandler.java
+++ board-app/app/src/main/java/com/bitcamp/board/handler/MemberHandler.java
@@ -37,8 +37,9 @@ public class MemberHandler {
         }
 
         displayBlankLine();
+
       } catch (Throwable ex) {
-        System.out.println("입력 값이 옳지 않습니다!");
+        System.out.printf("예외 발생: %s\n", ex.getMessage());
       }
     } // 게시판 while
   }
@@ -65,7 +66,7 @@ public class MemberHandler {
 
   }
 
-  private void onDetail() {
+  private void onDetail() throws Throwable {
     System.out.println("[회원 상세보기]");
 
     String email = Prompt.inputString("조회할 회원 이메일? ");
@@ -81,7 +82,6 @@ public class MemberHandler {
     System.out.printf("이메일: %s\n", member.email);
     Date date = new Date(member.createdDate);
     System.out.printf("등록일: %tY-%1$tm-%1$td %1$tH:%1$tM\n", date);
-
   }
 
   private void onInput() {
@@ -99,7 +99,7 @@ public class MemberHandler {
     System.out.println("회워을 등록했습니다.");
   }
 
-  private void onDelete() {
+  private void onDelete() throws Throwable {
     System.out.println("[회원 삭제]");
 
     String email = Prompt.inputString("삭제할 회원 이메일? ");
@@ -111,7 +111,7 @@ public class MemberHandler {
     }
   }
 
-  private void onUpdate() {
+  private void onUpdate() throws Throwable {
     System.out.println("[회원 변경]");
 
     String email = Prompt.inputString("변경할 회원 이메일? ");
