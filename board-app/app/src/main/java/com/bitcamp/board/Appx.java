b32aa565db05c2a637189938d41b82ad2efbf6a4
Jongchan Choi <drgrjongchan@gmail.com>
2022-08-01 20:35:07 +0900

    자바 실습

--- board-app/app/src/main/java/com/bitcamp/board/Appx.java
+++ board-app/app/src/main/java/com/bitcamp/board/Appx.java
@@ -1,42 +1,40 @@
 package com.bitcamp.board;
 
 
-//7/30까지 app005진행중. 여기서 날짜코드 확인하고 줄바꾸고 구분선 출력 확인 미흡
-// 게시판의 번호를 지정해주는 코드확인하기
+//8/1까지 app006진행중
 
 public class Appx {
+
+  static int boardCount = 0;//저장된 게시글의 개수
+
+  static final int SIZE =3; //메뉴수 상수이기때문에 변수대문자를 씀
+  static int[] no = new int[SIZE];
+  static String[] title = new String[SIZE];
+  static String[] content = new String[SIZE];
+  static String[] writer = new String[SIZE];
+  static String[] password = new String[SIZE];
+  static int[] viewCount = new int[SIZE];
+  static long[] createdDate = new long[SIZE];
+
+
   public static void main(String[] args) {
 
-    System.out.println("게시판 애플리케이션!");
-    System.out.println();
-    System.out.println("환영합니다!");
-    System.out.println();
+    welcome();
 
     java.util.Scanner keyboardInput = new java.util.Scanner(System.in);
     java.text.SimpleDateFormat formatter = 
         new java.text.SimpleDateFormat("yyy-MM-dd");
 
+    loop: while (true) {
+      displayMenu();
 
-    final int SIZE =3; //메뉴수 상수이기때문에 변수대문자를 씀
 
-    int[] no = new int[SIZE];
-    String[] title = new String[SIZE];
-    String[] content = new String[SIZE];
-    String[] writer = new String[SIZE];
-    String[] password = new String[SIZE];
-    int[] viewCount = new int[SIZE];
-    long[] createdDate = new long[SIZE];
+    }
 
-    int boardCount = 0;//저장된 게시글의 개수
 
 
     while (true) {
-      System.out.println("메뉴:");
-      System.out.println("  1: 게시글 목록");
-      System.out.println("  2: 게시글 상세보기");
-      System.out.println("  3: 게시글 등록");
-      System.out.println();
-      System.out.print("메뉴를 선택하세요[1..3](0: 종료)");
+
 
       int menuNo = keyboardInput.nextInt();
       keyboardInput.nextLine(); // 입력한 숫자뒤에 엔터를 치게되면 엔터값이 남아있게되므로 이때 있는 
@@ -122,6 +120,25 @@ public class Appx {
     keyboardInput.close();
 
   }
+  static void welcome() {
+    System.out.println("게시판 애플리케이션!");
+    System.out.println();
+    System.out.println("환영합니다!");
+    System.out.println();
+  }
+  static void displayMenu( ) {
+    System.out.println("메뉴:");
+    System.out.println("  1: 게시글 목록");
+    System.out.println("  2: 게시글 상세보기");
+    System.out.println("  3: 게시글 등록");
+    System.out.println();
+    System.out.print("메뉴를 선택하세요[1..3](0: 종료)");
+  }
+
+
+
+
+
 }
 
 
