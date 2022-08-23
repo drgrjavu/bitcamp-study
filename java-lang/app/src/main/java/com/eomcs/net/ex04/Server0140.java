66269248e0e39e0d8e8817eacaf8d51a185daa9a
Jongchan Choi <drgrjongchan@gmail.com>
2022-08-23 17:28:49 +0900

    공부자료

--- java-lang/app/src/main/java/com/eomcs/net/ex04/Server0140.java
+++ java-lang/app/src/main/java/com/eomcs/net/ex04/Server0140.java
@@ -24,16 +24,19 @@ public class Server0140 {
 
           System.out.println("클라이언트가 연결되었음!");
 
+          // 접속한 클라이언트의 IP 주소 알아내기
+          InetAddress inetAddr = socket.getInetAddress();
+          System.out.printf("접속자: %s\n", inetAddr.getHostAddress());
+
           while (true) {
             String name = in.readLine();
             if (name.equalsIgnoreCase("quit")) { // 클라이언트와 연결 끊기
               out.println("Goodbye!");
               out.flush();
               break;
-            } else if (name.equalsIgnoreCase("stop")) { // 서버 종료하기
-              // 접속한 클라이언트의 IP 주소 알아내기
-              InetAddress inetAddr = socket.getInetAddress();
-              System.out.println(inetAddr.getHostAddress()); 
+            } else if (name.equalsIgnoreCase("stop")
+                // localhost 에서만 서버를 멈출 수 있다.
+                && inetAddr.getHostAddress().equals("127.0.0.1")) { // 서버 종료하기
               out.println("Goodbye!");
               out.flush();
               break loop;
