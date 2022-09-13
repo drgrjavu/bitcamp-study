2caffc0c492d172d7ff0cf17ecb4ce547e5a5bb3
Jongchan Choi <drgrjongchan@gmail.com>
2022-09-13 17:33:05 +0900

    공부자료

--- board-app/app-client/src/main/java/com/bitcamp/board/ClientApp.java
+++ board-app/app-client/src/main/java/com/bitcamp/board/ClientApp.java
@@ -5,6 +5,11 @@ import java.io.DataOutputStream;
 import java.net.Socket;
 import com.bitcamp.util.Prompt;
 
+// 1) 서버에 접속 
+// 4) 사용자 입력을 서버에 전송
+// 5) 요청/응답을 무한 반복한다.
+// 6) quit 명령을 보내면 연결 끊기
+//
 public class ClientApp {
 
   public static void main(String[] args) {
@@ -15,10 +20,19 @@ public class ClientApp {
         DataInputStream in = new DataInputStream(socket.getInputStream());
         DataOutputStream out = new DataOutputStream(socket.getOutputStream())) {
 
-      String line = null;
+      String response = null;
 
-      line = in.readUTF();
-      System.out.println(line);
+      while (true) {
+        response = in.readUTF();
+        System.out.println(response);
+
+        String input = Prompt.inputString("> ");
+        out.writeUTF(input);
+
+        if (input.equals("quit")) {
+          break;
+        }
+      }
 
     } catch (Exception e) {
       System.out.println("서버와 통신 중 오류 발생!");
