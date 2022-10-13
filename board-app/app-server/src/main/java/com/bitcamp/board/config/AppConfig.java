e5bd7da7bb2a8c59167695c36a0f1b82b6ba2ae4
Jongchan Choi <drgrjongchan@gmail.com>
2022-10-13 17:42:55 +0900

    공부자료

--- board-app/app-server/src/main/java/com/bitcamp/board/config/AppConfig.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/config/AppConfig.java
@@ -6,6 +6,11 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 //스프링 IoC 컨테이너의 설정을 수행하는 클래스
 //1) DB 커넥션 객체 관리자 준비 : DataSource
@@ -47,4 +52,30 @@ public class AppConfig {
     return ds;
   }
 
+  // multipart/form-data 형식으로 보내온 요청 데이터를 
+  // 도메인 객체로 받는 일을 할 도우미 객체를 등록한다.
+  // 이 객체가 등록된 경우 multipart/form-data 를 도메인 객체로 받을 수 있다.
+  @Bean("multipartResolver")
+  public MultipartResolver createMultipartResolver() {
+    return new StandardServletMultipartResolver();
+  }
+
+  // Spring WebMVC 의 기본 ViewResolver를 교체한다.
+  @Bean("viewResolver")
+  public ViewResolver createViewResolver() {
+    InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+    viewResolver.setViewClass(JstlView.class); // 주어진 URL을 처리할 객체 => JSP를 실행시키주는 객체
+    viewResolver.setPrefix("/WEB-INF/jsp/");
+    viewResolver.setSuffix(".jsp");
+    return viewResolver;
+  }
+
 }
+
+
+
+
+
+
+
+
--- board-app/app-server/src/main/java/com/bitcamp/board/controller/AuthController.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/controller/AuthController.java
@@ -1,13 +1,13 @@
 package com.bitcamp.board.controller;
 
 import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import com.bitcamp.board.domain.Member;
 import com.bitcamp.board.service.MemberService;
 
@@ -20,40 +20,49 @@ public class AuthController {
     this.memberService = memberService;
   }
 
+  // InternalResourceViewResolver 설정 전
+  //  @GetMapping("form") 
+  //  public View form() throws Exception {
+  //    return new JstlView("/auth/form.jsp");
+  //  }
+
+  // InternalResourceViewResolver 설정 후
   @GetMapping("form") 
-  public String form(HttpServletRequest request, HttpServletResponse response) throws Exception {
-    return "/auth/form.jsp";
+  public String form() throws Exception {
+    return "auth/form";
   }
 
   @PostMapping("login") 
-  public String login(HttpServletRequest request, HttpServletResponse response) throws Exception {
-    String email = request.getParameter("email");
-    String password = request.getParameter("password");
+  public ModelAndView login(
+      String email, 
+      String password, 
+      String saveEmail, 
+      HttpServletResponse response,
+      HttpSession session) throws Exception {
 
     Member member = memberService.get(email, password);
 
     if (member != null) {
-      HttpSession session = request.getSession(); 
       session.setAttribute("loginMember", member); 
     }
 
     Cookie cookie = new Cookie("email", email); 
-    if (request.getParameter("saveEmail") == null) {
+    if (saveEmail == null) {
       cookie.setMaxAge(0); 
     } else {
       cookie.setMaxAge(60 * 60 * 24 * 7); // 7일
     }
     response.addCookie(cookie); 
 
-    request.setAttribute("member", member);
-    return "/auth/loginResult.jsp";
+    ModelAndView mv = new ModelAndView("auth/loginResult");
+    mv.addObject("member", member);
+    return mv;
   }
 
   @GetMapping("logout") 
-  public String logout(HttpServletRequest request, HttpServletResponse response) throws Exception {
-    HttpSession session = request.getSession();
-    session.invalidate(); // 현재 세션을 무효화시킨다.
-    return "redirect:../../"; // 로그아웃 한 후 메인 페이지를 요청하라고 응답한다.
+  public String logout(HttpSession session) throws Exception {
+    session.invalidate(); 
+    return "redirect:../../"; 
   }
 }
 
